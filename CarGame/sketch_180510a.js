3ea850137228de0cc220ac5714fcb3323b8e26f3
Riflescoop <alanator5000@gmail.com>
2018-05-14 18:08:56 -0400

    Update sketch_180510a.js

--- CarGame/sketch_180510a.js
+++ CarGame/sketch_180510a.js
@@ -320,14 +320,14 @@ function setup() {
 
 function congratulations() {
   image(congratulations_img,0,0,1900,900);
-  function keyPressed() {
+  if(mouseIsPressed) {
     congratulations_bool=false;
     explanation1_bool=true;
   }
 }
 function fail() {
   image(fail_img,0,0,1900,900);
-  function keyPressed() {
+  if(mouseIsPressed) {
     congratulations_bool=false;
     explanation1_bool=true;
   }
